3b259002377fb513af06b7f35245d5fd98d0f3a8
Andrea <ngocanhhsgs@gmail.com>
2023-01-03 23:57:44 +0200

    landing page

--- src/components/landingPage/LandingPage.tsx
+++ src/components/landingPage/LandingPage.tsx
@@ -1,15 +1,14 @@
 import React from "react";
 
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 
 export default function LandingPage() {
   return (
     <div className="landingPage">
-      <Typography sx={{ fontSize: 100, fontWeight: "bold" }}>Sale</Typography>{" "}
+      <Typography sx={{ fontSize: 100 }}>Sale</Typography>
       <br />
-      <Typography sx={{ fontSize: 70, fontWeight: "bold" }}>
-        Up to 50% off
-      </Typography>
+      <Typography sx={{ fontSize: 70 }}>Up to 50% off</Typography>
+      <Button variant="outlined"> Check out new collection</Button>
     </div>
   );
 }
