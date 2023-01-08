fa03589b0bc3bc98b6acd401f289f293c5513101
Andrea <ngocanhhsgs@gmail.com>
2023-01-08 21:39:55 +0200

    minor change

--- src/components/landingPage/LandingPage.tsx
+++ src/components/landingPage/LandingPage.tsx
@@ -8,7 +8,7 @@ export default function LandingPage() {
       <Typography sx={{ fontSize: 100 }}>Sale</Typography>
       <br />
       <Typography sx={{ fontSize: 70 }}>Up to 50% off</Typography>
-      <Button variant="outlined" sx={{ color: "black" }}>
+      <Button variant="outlined" sx={{ color: "black", mt: 7 }}>
         <Link className="link" to="/products">
           Check out new collection
         </Link>
